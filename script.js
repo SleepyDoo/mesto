c344f3abf1d6be33b67f5d3a6ff702dfb7be03c0
SleepyDoo <86569412+SleepyDoo@users.noreply.github.com>
2021-10-15 20:47:35 +0300

    Редактирование после второго коммита

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 let popup = document.querySelector('.popup');
 let editButton = document.querySelector('.profile__edit-button')
 let closeButton = document.querySelector('.popup__close-button')
+let inputName = document.querySelector('.form__input_content_name');
+let inputDescription = document.querySelector('.form__input_content_description');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
+let form = document.querySelector('.form');
 
 function openPopup() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
   popup.classList.add('popup_opened');
 }
-
-editButton.addEventListener('click', openPopup);
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', closePopup);
-
-let InputName = document.querySelector('.form__input_content_name');
-let InputDescription = document.querySelector('.form__input_content_description');
-let profileName = document.querySelector('.profile__name');
-let profileDescription = document.querySelector('.profile__description');
-let form = document.querySelector('.form');
-
-InputName.setAttribute('value', profileName.textContent);
-InputDescription.setAttribute('value', profileDescription.textContent);
-
 function submitProfileBio (evt) {
   evt.preventDefault();
-  profileName.textContent = InputName.value;
-  profileDescription.textContent = InputDescription.value;
-  popup.classList.remove('popup_opened');
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  closePopup();
 }
 
 form.addEventListener('submit', submitProfileBio);
+closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopup);
